9b8d9918e0bd9fffc44aff6d16060ac2fb57ae14
varunraiko <varunraiko1803@gmail.com>
2016-05-24 13:49:56 +0530

    is_aggregate flag added to sp_head

--- sql/sp_head.h
+++ sql/sp_head.h
@@ -174,6 +174,7 @@ public:
   };
 
   stored_procedure_type m_type;
+  bool is_aggregate;
   uint m_flags;                 // Boolean attributes of a stored routine
 
   Column_definition m_return_field_def; /**< This is used for FUNCTIONs only. */
