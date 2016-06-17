cf0286345985f2d67c9e1665d236aea62cd4cbe5
varunraiko <varunraiko1803@gmail.com>
2016-06-18 03:47:58 +0530

    introduced the callee memroot

--- sql/sp_head.h
+++ sql/sp_head.h
@@ -172,7 +172,7 @@ public:
     */
     HAS_COLUMN_TYPE_REFS= 8192
   };
-
+  
   stored_procedure_type m_type;
   uint m_flags;                 // Boolean attributes of a stored routine
 
@@ -771,9 +771,11 @@ public:
 private:
 
   MEM_ROOT *m_thd_root;		///< Temp. store for thd's mem_root
+  MEM_ROOT callee_mem_root;
   THD *m_thd;			///< Set if we have reset mem_root
 
   sp_pcontext *m_pcont;		///< Parse context
+  sp_rcontext *m_rcont;
   List<LEX> m_lex;		///< Temp. store for the other lex
   DYNAMIC_ARRAY m_instr;	///< The "instructions"
 
