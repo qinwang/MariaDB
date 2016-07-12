e95f605d871286ca284020bf138ef881c0299894
varunraiko <varunraiko1803@gmail.com>
2016-07-13 02:56:37 +0530

    updated commit for the item_sum_sp

--- sql/sp_head.h
+++ sql/sp_head.h
@@ -297,9 +297,6 @@ public:
     being opened is probably enough).
   */
   SQL_I_List<Item_trigger_field> m_trg_table_fields;
-  uint instr_ptr;
-  bool pause_state;
-  
 
   static void *
   operator new(size_t size) throw ();
@@ -338,7 +335,7 @@ public:
   
   bool
   execute_aggregate_function(THD *thd, Item **args, uint argcount,
-                             Field *return_fld, sp_rcontext *nctx,
+                             Field *return_fld, sp_rcontext **nctx,
                              MEM_ROOT *caller_mem_root);
 
   bool
@@ -775,6 +772,10 @@ public:
   }
 
   sp_pcontext *get_parse_context() { return m_pcont; }
+  void init_instr_ptr(int ip)
+  {
+    instr_ptr= ip;
+  }
 
 private:
 
@@ -818,6 +819,7 @@ private:
     in prelocked mode and in non-prelocked mode.
   */
   HASH m_sptabs;
+  uint instr_ptr;
 
   bool
   execute(THD *thd, bool merge_da_on_success);
