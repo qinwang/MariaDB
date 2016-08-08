c99f7981e7a3c8605db4272341a9e8bd450c3c25
Daniel Fiala <freelancer@meave.biz>
2016-08-08 04:37:46 +0200

    Update statement: Historical rows should be skipped on higher level.

--- sql/sql_update.cc
+++ sql/sql_update.cc
@@ -2115,8 +2115,8 @@ multi_update::~multi_update()
     {
       if (tmp_tables[cnt])
       {
-	free_tmp_table(thd, tmp_tables[cnt]);
-	tmp_table_param[cnt].cleanup();
+        free_tmp_table(thd, tmp_tables[cnt]);
+        tmp_table_param[cnt].cleanup();
       }
     }
   }
@@ -2152,6 +2152,11 @@ int multi_update::send_data(List<Item> &not_used_values)
     if (table->status & (STATUS_NULL_ROW | STATUS_UPDATED))
       continue;
 
+    if (table->s->with_system_versioning && !table->get_row_end_field()->is_max_timestamp())
+    {
+      continue;
+    }
+
     if (table == table_to_update)
     {
       /*
@@ -2164,11 +2169,6 @@ int multi_update::send_data(List<Item> &not_used_values)
 
       table->status|= STATUS_UPDATED;
       store_record(table,record[1]);
-      // XYZ: Can this happen?
-      if (table->s->with_system_versioning && !table->get_row_end_field()->is_max_timestamp())
-      {
-        continue;
-      }
 
       if (fill_record_n_invoke_before_triggers(thd, table,
                                                *fields_for_table[offset],
@@ -2286,6 +2286,7 @@ int multi_update::send_data(List<Item> &not_used_values)
       */
       uint field_num= 0;
       List_iterator_fast<TABLE> tbl_it(unupdated_check_opt_tables);
+      /* Set first tbl = table and then tbl to tables from tbl_it */
       TABLE *tbl= table;
       do
       {
