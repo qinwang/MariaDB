bfdfa9832cf7fc16bfd40fe91e48b4b7ad261b3a
Vladislav Vaintroub <wlad@mariadb.com>
2016-11-21 01:23:54 +0100

    do not build librarchive on systems with old cmake

--- storage/xtradb/xtrabackup/src/wsrep.cc
+++ storage/xtradb/xtrabackup/src/wsrep.cc
@@ -193,8 +193,8 @@ xb_write_galera_info()
 	if (fp == NULL) {
 
 		msg("xtrabackup: error: "
-		    "could not create "XB_GALERA_INFO_FILENAME", errno = %d\n",
-		    errno);
+		  "could not create " XB_GALERA_INFO_FILENAME ", errno = %d\n",
+		   errno);
 		exit(EXIT_FAILURE);
 	}
 
@@ -206,8 +206,8 @@ xb_write_galera_info()
 	if (fprintf(fp, "%s:%lld", uuid_str, (long long) seqno) < 0) {
 
 		msg("xtrabackup: error: "
-		    "could not write to "XB_GALERA_INFO_FILENAME", errno = %d\n",
-		    errno);
+		 "could not write to " XB_GALERA_INFO_FILENAME ", errno = %d\n",
+		  errno);
 		exit(EXIT_FAILURE);
 	}
 
