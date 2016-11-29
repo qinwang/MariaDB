f50b4550fa71d41e095aef67cfb35701e3ec4d53
Vladislav Vaintroub <wlad@mariadb.com>
2016-11-29 22:52:21 +0000

    Do not create default xtrabackup_backupfiles dir, if streaming is requested

--- storage/xtradb/xtrabackup/src/xtrabackup.cc
+++ storage/xtradb/xtrabackup/src/xtrabackup.cc
@@ -3896,7 +3896,8 @@ xtrabackup_backup_func(void)
 	}
 
 	/* create target dir if not exist */
-	if (!my_stat(xtrabackup_target_dir,&stat_info,MYF(0))
+	if (!xtrabackup_stream &&
+		my_stat(xtrabackup_target_dir, &stat_info, MYF(0))
 		&& (my_mkdir(xtrabackup_target_dir,0777,MYF(0)) < 0)){
 		msg("xtrabackup: Error: cannot mkdir %d: %s\n",
 		    my_errno, xtrabackup_target_dir);
