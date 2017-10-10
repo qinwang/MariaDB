67181f88f411e4733a837776f98da4073a8c52e4
sjaakola <seppo.jaakola@iki.fi>
2017-10-10 23:39:48 +0300

    MW-416 Replicating DDL after ACL check, 5.6 version
    Re-implemented the fix for MW-416 according to 5.7 version

--- sql/sql_plugin.cc
+++ sql/sql_plugin.cc
@@ -2163,6 +2163,7 @@ bool mysql_install_plugin(THD *thd, const LEX_CSTRING *name,
   tables.init_one_table("mysql", 5, "plugin", 6, "plugin", TL_WRITE);
   if (!opt_noacl && check_table_access(thd, INSERT_ACL, &tables, FALSE, 1, FALSE))
     DBUG_RETURN(TRUE);
+  WSREP_TO_ISOLATION_BEGIN(WSREP_MYSQL_DB, NULL, NULL);
 
   WSREP_TO_ISOLATION_BEGIN(WSREP_MYSQL_DB, NULL, NULL)
 
@@ -2289,6 +2290,10 @@ static bool do_uninstall(THD *thd, TABLE *table, const LEX_CSTRING *name)
     }
   }
   return 0;
+#ifdef WITH_WSREP
+ error:
+#endif /* WITH_WSREP */
+  return 1;
 }
 
 
@@ -2307,6 +2312,7 @@ bool mysql_uninstall_plugin(THD *thd, const LEX_CSTRING *name,
 
   if (!opt_noacl && check_table_access(thd, DELETE_ACL, &tables, FALSE, 1, FALSE))
     DBUG_RETURN(TRUE);
+  WSREP_TO_ISOLATION_BEGIN(WSREP_MYSQL_DB, NULL, NULL);
 
   WSREP_TO_ISOLATION_BEGIN(WSREP_MYSQL_DB, NULL, NULL)
 
