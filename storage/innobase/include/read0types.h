MariaDB/server

commit bc7a1dc1fbd27e6064d3b40443fe242397668af7

MDEV-15104 - Optimise MVCC snapshot
With trx_sys_t::rw_trx_ids removal, MVCC snapshot overhead became
slightly higher. That is instead of copying an array we now have to
iterate LF_HASH. All this done under trx_sys.mutex protection.

This patch moves MVCC snapshot out of trx_sys.mutex.

Clean-ups:

Removed MVCC: doesn't make too much sense to keep it in a separate class
anymore.

Refactored ReadView so that it now calls register()/deregister() routines
(it was vice versa before).

ReadView doesn't have friends anymore. :(

Even less trx_sys.mutex references.

storage/innobase/include/read0types.h
@@ -32,16 +32,154 @@ Created 2/16/1997 Heikki Tuuri
 
 #include "trx0types.h"
 
-// Friend declaration
-class MVCC;
 
-/** Read view lists the trx ids of those transactions for which a consistent
-read should not see the modifications to the database. */
+/** View is not in MVCC and not visible to purge thread. */
+#define READ_VIEW_STATE_CLOSED 0
 
-class ReadView {
+/** View is in MVCC, but not visible to purge thread. */
+#define READ_VIEW_STATE_REGISTERED 1
+
+/** View is in MVCC, purge thread must wait for READ_VIEW_STATE_OPEN. */
+#define READ_VIEW_STATE_SNAPSHOT 2
+
+/** View is in MVCC and is visible to purge thread. */
+#define READ_VIEW_STATE_OPEN 3
+
+
+/**
+  Read view lists the trx ids of those transactions for which a consistent read
+  should not see the modifications to the database.
+*/
+class ReadView
+{
+  /**
+    View state.
+
+    It is not defined as enum as it has to be updated using atomic operations.
+    Possible values are READ_VIEW_STATE_CLOSED, READ_VIEW_STATE_REGISTERED,
+    READ_VIEW_STATE_SNAPSHOT and READ_VIEW_STATE_OPEN.
+
+    Possible state transfers...
+
+    Opening view for the first time:
+    READ_VIEW_STATE_CLOSED -> READ_VIEW_STATE_SNAPSHOT (non-atomic)
+
+    Complete first time open or reopen:
+    READ_VIEW_STATE_SNAPSHOT -> READ_VIEW_STATE_OPEN (atomic)
+
+    Close view but keep it in list:
+    READ_VIEW_STATE_OPEN -> READ_VIEW_STATE_REGISTERED (atomic)
+
+    Close view and remove it from list:
+    READ_VIEW_STATE_OPEN -> READ_VIEW_STATE_CLOSED (non-atomic)
+
+    Reusing view:
+    READ_VIEW_STATE_REGISTERED -> READ_VIEW_STATE_SNAPSHOT (atomic)
+
+    Removing closed view from list:
+    READ_VIEW_STATE_REGISTERED -> READ_VIEW_STATE_CLOSED (non-atomic)
+  */
+  int32_t m_state;
+
+
 public:
-	ReadView() : m_creator_trx_id(TRX_ID_MAX), m_ids(),
-		     m_registered(false) {}
+  ReadView(): m_state(READ_VIEW_STATE_CLOSED) {}
+
+
+  /**
+    Copy state from another view.
+
+    @param other    view to copy from
+  */
+  void copy(const ReadView &other)
+  {
+    ut_ad(&other != this);
+    m_ids= other.m_ids;
+    m_up_limit_id= other.m_up_limit_id;
+    m_low_limit_no= other.m_low_limit_no;
+    m_low_limit_id= other.m_low_limit_id;
+  }
+
+
+  /**
+    Opens a read view where exactly the transactions serialized before this
+    point in time are seen in the view.
+
+    View becomes visible to purge thread via trx_sys.m_views.
+
+    @param[in,out] trx transaction
+  */
+  void open(trx_t *trx);
+
+
+  /**
+    Closes the view.
+
+    View becomes not visible to purge thread via trx_sys.m_views.
+  */
+  void close();
+
+
+  /**
+    Marks view unused.
+
+    View is still in trx_sys.m_views list, but is not visible to purge threads.
+  */
+  void unuse()
+  {
+    ut_ad(m_state == READ_VIEW_STATE_CLOSED ||
+          m_state == READ_VIEW_STATE_REGISTERED ||
+          m_state == READ_VIEW_STATE_OPEN);
+    if (m_state == READ_VIEW_STATE_OPEN)
+      my_atomic_store32_explicit(&m_state, READ_VIEW_STATE_REGISTERED,
+                                 MY_MEMORY_ORDER_RELAXED);
+  }
+
+
+  /** m_state getter for trx_sys::clone_oldest_view() trx_sys::size(). */
+  int32_t get_state() const
+  {
+    return my_atomic_load32_explicit(const_cast<int32*>(&m_state),
+                                     MY_MEMORY_ORDER_ACQUIRE);
+  }
+
+
+  /**
+    Returns true if view is open.
+
+    Only used by view owner thread, thus we can omit atomic operations.
+  */
+  bool is_open() const
+  {
+    ut_ad(m_state == READ_VIEW_STATE_OPEN ||
+          m_state == READ_VIEW_STATE_CLOSED ||
+          m_state == READ_VIEW_STATE_REGISTERED);
+    return m_state == READ_VIEW_STATE_OPEN;
+  }
+
+
+  /**
+    Creates a snapshot where exactly the transactions serialized before this
+    point in time are seen in the view.
+
+    @param[in,out] trx transaction
+  */
+  void snapshot(trx_t *trx);
+
+
+  /**
+    Sets the creator transaction id.
+
+    This should be set only for views created by RW transactions.
+  */
+  void set_creator_trx_id(trx_id_t id)
+  {
+    ut_ad(id > 0);
+    ut_ad(m_creator_trx_id == 0);
+    m_creator_trx_id= id;
+  }
+
+
 	/** Check whether transaction id is valid.
 	@param[in]	id		transaction id to check
 	@param[in]	name		table name */
@@ -86,25 +224,6 @@ public:
 	}
 
 	/**
-	Mark the view as closed */
-	void close()
-	{
-		set_creator_trx_id(TRX_ID_MAX);
-	}
-
-	bool is_open() const
-	{
-		return static_cast<trx_id_t>(my_atomic_load64_explicit(
-				const_cast<int64*>(
-				reinterpret_cast<const int64*>(
-				&m_creator_trx_id)),
-				MY_MEMORY_ORDER_RELAXED)) != TRX_ID_MAX;
-	}
-
-	bool is_registered() const { return(m_registered); }
-	void set_registered(bool registered) { m_registered= registered; }
-
-	/**
 	Write the limits to the file.
 	@param file		file to write to */
 	void print_limits(FILE* file) const
@@ -129,54 +248,8 @@ public:
 		return(m_low_limit_id);
 	}
 
-	/**
-	@return true if there are no transaction ids in the snapshot */
-	bool empty() const
-	{
-		return(m_ids.empty());
-	}
 
-	/**
-	Set the creator transaction id, existing id must be 0.
-
-	Note: This shouldbe set only for views created by RW
-	transactions. */
-	void set_creator_trx_id(trx_id_t id)
-	{
-		my_atomic_store64_explicit(
-				reinterpret_cast<int64*>(&m_creator_trx_id),
-				id, MY_MEMORY_ORDER_RELAXED);
-	}
-
-#ifdef UNIV_DEBUG
-	/**
-	@param rhs		view to compare with
-	@return truen if this view is less than or equal rhs */
-	bool le(const ReadView* rhs) const
-	{
-		return(m_low_limit_no <= rhs->m_low_limit_no);
-	}
-
-	trx_id_t up_limit_id() const
-	{
-		return(m_up_limit_id);
-	}
-#endif /* UNIV_DEBUG */
 private:
-	/**
-	Opens a read view where exactly the transactions serialized before this
-	point in time are seen in the view.
-
-	@param[in,out] trx transaction */
-	void open(trx_t *trx);
-
-	/**
-	Copy state from another view.
-	@param other		view to copy from */
-	inline void copy(const ReadView& other);
-
-	friend class MVCC;
-
 	/** The read should not see any transaction with trx id >= this
 	value. In other words, this is the "high water mark". */
 	trx_id_t	m_low_limit_id;
@@ -199,11 +272,8 @@ private:
 	they can be removed in purge if not needed by other views */
 	trx_id_t	m_low_limit_no;
 
-	/** true if transaction is in MVCC::m_views. Only thread that owns
-	this view may access it. */
-	bool		m_registered;
-
 	byte		pad1[CACHE_LINE_SIZE];
+public:
 	UT_LIST_NODE_T(ReadView)	m_view_list;
 };
 

storage/innobase/include/trx0sys.h
@@ -810,17 +810,17 @@ private:
   /** Solves race condition between register_rw() and snapshot_ids(). */
   MY_ALIGNED(CACHE_LINE_SIZE) trx_id_t m_rw_trx_hash_version;
 
+
+  /** Active views. */
+  MY_ALIGNED(CACHE_LINE_SIZE) UT_LIST_BASE_NODE_T(ReadView) m_views;
+
   bool m_initialised;
 
 public:
-	MY_ALIGNED(CACHE_LINE_SIZE)
+	MY_ALIGNED(CACHE_LINE_SIZE) mutable
 	TrxSysMutex	mutex;		/*!< mutex protecting most fields in
 					this structure except when noted
 					otherwise */
-
-	MY_ALIGNED(CACHE_LINE_SIZE)
-	MVCC		mvcc;		/*!< Multi version concurrency control
-					manager */
 	MY_ALIGNED(CACHE_LINE_SIZE)
 	trx_ut_list_t	mysql_trx_list;	/*!< List of transactions created
 					for MySQL. All user transactions are
@@ -1029,6 +1029,59 @@ public:
   trx_t *find(trx_t *caller_trx, trx_id_t id)
   {
     return rw_trx_hash.find(caller_trx, id, true);
+  }
+
+
+  /**
+    Registers view in MVCC.
+
+    @param view    view owned by the caller
+  */
+  void register_view(ReadView *view)
+  {
+    mutex_enter(&mutex);
+    UT_LIST_ADD_FIRST(m_views, view);
+    mutex_exit(&mutex);
+  }
+
+
+  /**
+    Deregisters view in MVCC.
+
+    @param view    view owned by the caller
+  */
+  void deregister_view(ReadView *view)
+  {
+    mutex_enter(&mutex);
+    UT_LIST_REMOVE(m_views, view);
+    mutex_exit(&mutex);
+  }
+
+
+  /**
+    Clones the oldest view and stores it in view.
+
+    No need to call ReadView::close(). The caller owns the view that is passed
+    in. This function is called by purge thread to determine whether it should
+    purge the delete marked record or not.
+  */
+  void clone_oldest_view();
+
+
+  /** @return the number of active views */
+  size_t view_count() const
+  {
+    size_t count= 0;
+
+    mutex_enter(&mutex);
+    for (const ReadView* view= UT_LIST_GET_FIRST(m_views); view;
+         view= UT_LIST_GET_NEXT(m_view_list, view))
+    {
+      if (view->get_state() == READ_VIEW_STATE_OPEN)
+        ++count;
+    }
+    mutex_exit(&mutex);
+    return count;
   }
 
 

storage/innobase/read/read0read.cc
@@ -24,10 +24,11 @@ Cursor read
 Created 2/16/1997 Heikki Tuuri
 *******************************************************/
 
-#include "read0read.h"
+#include "read0types.h"
 
 #include "srv0srv.h"
 #include "trx0sys.h"
+#include "trx0purge.h"
 
 /*
 -------------------------------------------------------------------------------
@@ -172,52 +173,16 @@ RW transaction can commit or rollback (or free views). AC-NL-RO transactions
 will mark their views as closed but not actually free their views.
 */
 
-#ifdef UNIV_DEBUG
-/** Functor to validate the view list. */
-struct	ViewCheck {
-
-	ViewCheck() : m_prev_view() { }
-
-	void	operator()(const ReadView* view)
-	{
-		ut_ad(view->is_registered());
-		ut_a(m_prev_view == NULL
-		     || !view->is_open()
-		     || view->le(m_prev_view));
-
-		m_prev_view = view;
-	}
-
-	const ReadView*	m_prev_view;
-};
 
 /**
-Validates a read view list. */
-
-bool
-MVCC::validate() const
-{
-	ViewCheck	check;
-
-	ut_ad(mutex_own(&trx_sys.mutex));
-
-	ut_list_map(m_views, check);
-
-	return(true);
-}
-#endif /* UNIV_DEBUG */
-
-
-/**
-  Opens a read view where exactly the transactions serialized before this
+  Creates a snapshot where exactly the transactions serialized before this
   point in time are seen in the view.
 
   @param[in,out] trx transaction
 */
-
-void ReadView::open(trx_t *trx)
+void ReadView::snapshot(trx_t *trx)
 {
-  ut_ad(mutex_own(&trx_sys.mutex));
+  ut_ad(!mutex_own(&trx_sys.mutex));
   trx_sys.snapshot_ids(trx, &m_ids, &m_low_limit_id, &m_low_limit_no);
   m_up_limit_id= m_ids.empty() ? m_low_limit_id : m_ids.front();
   ut_ad(m_up_limit_id <= m_low_limit_id);
@@ -225,42 +190,33 @@ void ReadView::open(trx_t *trx)
 
 
 /**
-  Create a view.
+  Opens a read view where exactly the transactions serialized before this
+  point in time are seen in the view.
 
-  Assigns a read view for a consistent read query. All the consistent reads
-  within the same transaction will get the same read view, which is created
-  when this function is first called for a new started transaction.
+  View becomes visible to purge thread via trx_sys.m_views.
 
-  @param trx   transaction instance of caller
+  @param[in,out] trx transaction
 */
-
-void MVCC::view_open(trx_t* trx)
+void ReadView::open(trx_t *trx)
 {
-  if (srv_read_only_mode)
+  ut_ad(this == &trx->read_view);
+  switch (m_state)
   {
-    ut_ad(!trx->read_view.is_open());
+  case READ_VIEW_STATE_OPEN:
+    ut_ad(!srv_read_only_mode);
     return;
-  }
-  else if (trx->read_view.is_open())
-    return;
-
-  /*
-    Reuse closed view if there were no read-write transactions since (and at) it's
-    creation time.
-  */
-  if (trx->read_view.is_registered() &&
-      trx_is_autocommit_non_locking(trx) &&
-      trx->read_view.empty() &&
-      trx->read_view.m_low_limit_id == trx_sys.get_max_trx_id())
-  {
+  case READ_VIEW_STATE_REGISTERED:
+    ut_ad(!srv_read_only_mode);
     /*
+      Reuse closed view if there were no read-write transactions since (and at)
+      its creation time.
+
       Original comment states: there is an inherent race here between purge
       and this thread.
 
       To avoid this race we should've checked trx_sys.get_max_trx_id() and
-      do trx->read_view.set_creator_trx_id(trx->id) atomically under
-      trx_sys.mutex protection. But we're cutting edges to achieve great
-      scalability.
+      set state to READ_VIEW_STATE_OPEN atomically under trx_sys.mutex
+      protection. But we're cutting edges to achieve great scalability.
 
       There're at least two types of concurrent threads interested in this
       value: purge coordinator thread (see MVCC::clone_oldest_view()) and
@@ -280,108 +236,108 @@ void MVCC::view_open(trx_t* trx)
       Second, scary things start when there's a read-write transaction starting
       concurrently.
 
-      Speculative execution may reorder set_creator_trx_id() before
-      get_max_trx_id(). In this case purge thread has short gap to clone
-      outdated view. Which is probably not that bad: it just won't be able to
-      purge things that it was actually allowed to purge for a short while.
+      Speculative execution may reorder state change before get_max_trx_id().
+      In this case purge thread has short gap to clone outdated view. Which is
+      probably not that bad: it just won't be able to purge things that it was
+      actually allowed to purge for a short while.
 
       This thread may as well get suspended after trx_sys.get_max_trx_id() and
-      before trx->read_view.set_creator_trx_id(trx->id). New read-write
-      transaction may get started, committed and purged meanwhile. It is
-      acceptable as well, since this view doesn't see it.
+      before state is set to READ_VIEW_STATE_OPEN. New read-write transaction
+      may get started, committed and purged meanwhile. It is acceptable as
+      well, since this view doesn't see it.
     */
-    trx->read_view.set_creator_trx_id(trx->id);
-    return;
-  }
+    if (trx_is_autocommit_non_locking(trx) && m_ids.empty() &&
+        m_low_limit_id == trx_sys.get_max_trx_id())
+      goto reopen;
 
-  mutex_enter(&trx_sys.mutex);
-  trx->read_view.open(trx);
-  if (trx->read_view.is_registered())
-    UT_LIST_REMOVE(m_views, &trx->read_view);
-  else
-    trx->read_view.set_registered(true);
-  trx->read_view.set_creator_trx_id(trx->id);
-  UT_LIST_ADD_FIRST(m_views, &trx->read_view);
-  ut_ad(validate());
-  mutex_exit(&trx_sys.mutex);
-}
+    /*
+      Can't reuse view, take new snapshot.
 
+      Alas this empty critical section is simplest way to make sure concurrent
+      purge thread completed snapshot copy. Of course purge thread may come
+      again and try to copy once again after we release this mutex, but in
+      this case it is guaranteed to see READ_VIEW_STATE_REGISTERED and thus
+      it'll skip this view.
 
-void MVCC::view_close(ReadView &view)
-{
-  view.close();
-  if (view.is_registered())
-  {
+      This critical section can be replaced with new state, which purge thread
+      would set to inform us to wait until it completes snapshot. However it'd
+      complicate m_state even further.
+    */
     mutex_enter(&trx_sys.mutex);
-    view.set_registered(false);
-    UT_LIST_REMOVE(m_views, &view);
-    ut_ad(validate());
     mutex_exit(&trx_sys.mutex);
+    my_atomic_store32_explicit(&m_state, READ_VIEW_STATE_SNAPSHOT,
+                               MY_MEMORY_ORDER_RELAXED);
+    break;
+  case READ_VIEW_STATE_CLOSED:
+    if (srv_read_only_mode)
+      return;
+    m_state= READ_VIEW_STATE_SNAPSHOT;
+    trx_sys.register_view(this);
+    break;
+  default:
+    ut_ad(0);
+  }
+
+  snapshot(trx);
+reopen:
+  m_creator_trx_id= trx->id;
+  my_atomic_store32_explicit(&m_state, READ_VIEW_STATE_OPEN,
+                             MY_MEMORY_ORDER_RELEASE);
+}
+
+
+/**
+  Closes the view.
+
+  The view will become invisible to purge (deregistered from trx_sys).
+*/
+void ReadView::close()
+{
+  ut_ad(m_state == READ_VIEW_STATE_OPEN ||
+        m_state == READ_VIEW_STATE_REGISTERED ||
+        m_state == READ_VIEW_STATE_CLOSED);
+  if (m_state != READ_VIEW_STATE_CLOSED)
+  {
+    trx_sys.deregister_view(this);
+    m_state= READ_VIEW_STATE_CLOSED;
   }
 }
 
 
 /**
-Copy state from another view.
-@param other		view to copy from */
+  Clones the oldest view and stores it in view.
 
-void
-ReadView::copy(const ReadView& other)
+  No need to call ReadView::close(). The caller owns the view that is passed
+  in. This function is called by purge thread to determine whether it should
+  purge the delete marked record or not.
+
+  Since foreign views are accessed under the mutex protection, the only
+  possible state transfers are
+  READ_VIEW_STATE_SNAPSHOT -> READ_VIEW_STATE_OPEN
+  READ_VIEW_STATE_OPEN -> READ_VIEW_STATE_REGISTERED
+  All other state transfers are eliminated by the mutex.
+*/
+void trx_sys_t::clone_oldest_view()
 {
-	ut_ad(&other != this);
-	m_ids= other.m_ids;
-	m_up_limit_id = other.m_up_limit_id;
-	m_low_limit_no = other.m_low_limit_no;
-	m_low_limit_id = other.m_low_limit_id;
-}
+  const ReadView *oldest_view= &purge_sys->view;
 
-/** Clones the oldest view and stores it in view. No need to
-call view_close(). The caller owns the view that is passed in.
-This function is called by Purge to determine whether it should
-purge the delete marked record or not.
-@param view		Preallocated view, owned by the caller */
+  purge_sys->view.snapshot(0);
 
-void
-MVCC::clone_oldest_view(ReadView* view)
-{
-	mutex_enter(&trx_sys.mutex);
-	/* Find oldest view. */
-	for (const ReadView *oldest_view = UT_LIST_GET_LAST(m_views);
-	     oldest_view != NULL;
-	     oldest_view = UT_LIST_GET_PREV(m_view_list, oldest_view))
-	{
-		if (oldest_view->is_open())
-		{
-			view->copy(*oldest_view);
-			mutex_exit(&trx_sys.mutex);
-			return;
-		}
-	}
-	/* No views in the list: snapshot current state. */
-	view->open(0);
-	mutex_exit(&trx_sys.mutex);
-}
+  mutex_enter(&mutex);
+  /* Find oldest view. */
+  for (const ReadView *v= UT_LIST_GET_FIRST(m_views); v;
+       v= UT_LIST_GET_NEXT(m_view_list, v))
+  {
+    int32_t state;
 
-/**
-@return the number of active views */
+    while ((state= v->get_state()) == READ_VIEW_STATE_SNAPSHOT)
+      ut_delay(1);
 
-size_t
-MVCC::size() const
-{
-	mutex_enter(&trx_sys.mutex);
-
-	size_t	size = 0;
-
-	for (const ReadView* view = UT_LIST_GET_FIRST(m_views);
-	     view != NULL;
-	     view = UT_LIST_GET_NEXT(m_view_list, view)) {
-
-		if (view->is_open()) {
-			++size;
-		}
-	}
-
-	mutex_exit(&trx_sys.mutex);
-
-	return(size);
+    if (state == READ_VIEW_STATE_OPEN &&
+        v->low_limit_no() < oldest_view->low_limit_no())
+      oldest_view= v;
+  }
+  if (oldest_view != &purge_sys->view)
+    purge_sys->view.copy(*oldest_view);
+  mutex_exit(&mutex);
 }

storage/innobase/trx/trx0trx.cc
@@ -39,7 +39,6 @@ Created 3/26/1996 Heikki Tuuri
 #include "log0log.h"
 #include "os0proc.h"
 #include "que0que.h"
-#include "read0read.h"
 #include "srv0mon.h"
 #include "srv0srv.h"
 #include "fsp0sysspace.h"
@@ -511,7 +510,7 @@ trx_free(trx_t*& trx)
 	trx->mod_tables.clear();
 
 	ut_ad(!trx->read_view.is_open());
-	trx_sys.mvcc.view_close(trx->read_view);
+	trx->read_view.close();
 
 	/* trx locking state should have been reset before returning trx
 	to pool */
@@ -673,7 +672,7 @@ trx_disconnect_from_mysql(
 	trx_t*	trx,
 	bool	prepared)
 {
-	trx_sys.mvcc.view_close(trx->read_view);
+	trx->read_view.close();
 
 	mutex_enter(&trx_sys.mutex);
 
@@ -972,7 +971,7 @@ trx_lists_init_at_db_start()
 
 		ib::info() << "Trx id counter is " << trx_sys.get_max_trx_id();
 	}
-	trx_sys.mvcc.clone_oldest_view(&purge_sys->view);
+	trx_sys.clone_oldest_view();
 }
 
 /** Assign a persistent rollback segment in a round-robin fashion,
@@ -1512,7 +1511,7 @@ trx_commit_in_memory(
 				the transaction did not modify anything */
 {
 	trx->must_flush_log_later = false;
-	trx->read_view.close();
+	trx->read_view.unuse();
 
 	if (trx_is_autocommit_non_locking(trx)) {
 		ut_ad(trx->id == 0);

qinwang/MariaDB

commit 5b27354deb6e1a81f44e725259ff02813055d061

MDEV-15246 - premature history data deletion
This is regression after bc7a1dc1fbd27e6064d3b40443fe242397668af7 of
MDEV-15104 - Optimise MVCC snapshot.

Aforementioned revision removes mutex lock around ReadView creation,
which allows views to be created concurrently. Effectively it
invalidates "oldest view" approach: no single view can be considered
oldest anymore. Instead we have to iterate trx_sys.m_views to find
min(m_low_limit_no), min(m_low_limit_id) and all transaction ids below
min(m_low_limit_id), which would form oldest view.

Second regression comes from c0d5d7c0efb6d2b9961da7fb813ff000a5d3e4c5
of MDEV-15104 - Optimise MVCC snapshot.

It removes mutex protection around trx->no assignment, which opens up
a gap between m_max_trx_id increment and transaction serialisation
number becoming visible through rw_trx_hash. While we're in this gap
concurrent thread may come and do MVCC snapshot without seeing allocated
but not yet assigned serialisation number. Then at some point purge
thread may clone this view. As a result it won't see newly allocated
serialisation number and may remove "unnecessary" history data of this
transaction from rollback segments.

storage/innobase/include/read0types.h
@@ -89,15 +89,46 @@ public:
   /**
     Copy state from another view.
 
+    This method is used to find min(m_low_limit_no), min(m_low_limit_id) and
+    all transaction ids below min(m_low_limit_id). These values effectively
+    form oldest view.
+
     @param other    view to copy from
   */
   void copy(const ReadView &other)
   {
     ut_ad(&other != this);
-    m_ids= other.m_ids;
-    m_up_limit_id= other.m_up_limit_id;
-    m_low_limit_no= other.m_low_limit_no;
-    m_low_limit_id= other.m_low_limit_id;
+    if (m_low_limit_no > other.m_low_limit_no)
+      m_low_limit_no= other.m_low_limit_no;
+    if (m_low_limit_id > other.m_low_limit_id)
+      m_low_limit_id= other.m_low_limit_id;
+
+    trx_ids_t::iterator dst= m_ids.begin();
+    for (trx_ids_t::const_iterator src= other.m_ids.begin();
+         src != other.m_ids.end(); src++)
+    {
+      if (*src >= m_low_limit_id)
+        break;
+loop:
+      if (dst == m_ids.end())
+      {
+        m_ids.push_back(*src);
+        dst= m_ids.end();
+        continue;
+      }
+      if (*dst < *src)
+      {
+        dst++;
+        goto loop;
+      }
+      else if (*dst > *src)
+        dst= m_ids.insert(dst, *src) + 1;
+    }
+    m_ids.erase(std::lower_bound(dst, m_ids.end(), m_low_limit_id),
+                m_ids.end());
+
+    m_up_limit_id= m_ids.empty() ? m_low_limit_id : m_ids.front();
+    ut_a(m_up_limit_id <= m_low_limit_id); /* change to ut_ad!!! */
   }
 
 

storage/innobase/include/trx0sys.h
@@ -797,17 +797,23 @@ public:
 
 
 /** The transaction system central memory data structure. */
-struct trx_sys_t {
-private:
+class trx_sys_t
+{
   /**
     The smallest number not yet assigned as a transaction id or transaction
     number. Accessed and updated with atomic operations.
   */
-
   MY_ALIGNED(CACHE_LINE_SIZE) trx_id_t m_max_trx_id;
 
 
-  /** Solves race condition between register_rw() and snapshot_ids(). */
+  /**
+    Solves race conditions between register_rw() and snapshot_ids() as well as
+    race condition between assign_new_trx_no() and snapshot_ids().
+
+    @sa register_rw()
+    @sa assign_new_trx_no()
+    @sa snapshot_ids()
+  */
   MY_ALIGNED(CACHE_LINE_SIZE) trx_id_t m_rw_trx_hash_version;
 
 
@@ -895,7 +901,7 @@ public:
             next call to trx_sys.get_new_trx_id()
   */
 
-  trx_id_t get_max_trx_id(void)
+  trx_id_t get_max_trx_id()
   {
     return static_cast<trx_id_t>
            (my_atomic_load64_explicit(reinterpret_cast<int64*>(&m_max_trx_id),
@@ -917,13 +923,45 @@ public:
 
 
   /**
+    Allocates and assigns new transaction serialisation number.
+
+    There's a gap between m_max_trx_id increment and transaction serialisation
+    number becoming visible through rw_trx_hash. While we're in this gap
+    concurrent thread may come and do MVCC snapshot without seeing allocated
+    but not yet assigned serialisation number. Then at some point purge thread
+    may clone this view. As a result it won't see newly allocated serialisation
+    number and may remove "unnecessary" history data of this transaction from
+    rollback segments.
+
+    m_rw_trx_hash_version is intended to solve this problem. MVCC snapshot has
+    to wait until m_max_trx_id == m_rw_trx_hash_version, which effectively
+    means that all transaction serialisation numbers up to m_max_trx_id are
+    available through rw_trx_hash.
+
+    We rely on refresh_rw_trx_hash_version() to issue RELEASE memory barrier so
+    that m_rw_trx_hash_version increment happens after
+    trx->rw_trx_hash_element->no becomes visible through rw_trx_hash.
+
+    @param trx transaction
+  */
+  void assign_new_trx_no(trx_t *trx)
+  {
+    trx->no= get_new_trx_id_no_refresh();
+    my_atomic_store64_explicit(reinterpret_cast<int64*>
+                               (&trx->rw_trx_hash_element->no),
+                               trx->no, MY_MEMORY_ORDER_RELAXED);
+    refresh_rw_trx_hash_version();
+  }
+
+
+  /**
     Takes MVCC snapshot.
 
     To reduce malloc probablility we reserver rw_trx_hash.size() + 32 elements
     in ids.
 
     For details about get_rw_trx_hash_version() != get_max_trx_id() spin
-    @sa register_rw().
+    @sa register_rw() and @sa assign_new_trx_no().
 
     We rely on get_rw_trx_hash_version() to issue ACQUIRE memory barrier so
     that loading of m_rw_trx_hash_version happens before accessing rw_trx_hash.
@@ -941,6 +979,7 @@ public:
   void snapshot_ids(trx_t *caller_trx, trx_ids_t *ids, trx_id_t *max_trx_id,
                     trx_id_t *min_trx_no)
   {
+    ut_ad(!mutex_own(&mutex));
     snapshot_ids_arg arg(ids);
 
     while ((arg.m_id= get_rw_trx_hash_version()) != get_max_trx_id())
@@ -952,7 +991,6 @@ public:
     rw_trx_hash.iterate(caller_trx,
                         reinterpret_cast<my_hash_walk_action>(copy_one_id),
                         &arg);
-    std::sort(ids->begin(), ids->end());
 
     *max_trx_id= arg.m_id;
     *min_trx_no= arg.m_no;
@@ -1146,11 +1184,12 @@ private:
   /**
     Allocates new transaction id without refreshing rw_trx_hash version.
 
-    This method is extracted for exclusive use by register_rw() where
-    transaction must be inserted into rw_trx_hash between new transaction id
-    allocation and rw_trx_hash version refresh.
+    This method is extracted for exclusive use by register_rw() and
+    assign_new_trx_no() where new id must be allocated atomically with
+    payload of these methods from MVCC snapshot point of view.
 
     @sa get_new_trx_id()
+    @sa assign_new_trx_no()
 
     @return new transaction id
   */

storage/innobase/include/trx0types.h
@@ -112,8 +112,6 @@ enum trx_dict_op_t {
 struct trx_t;
 /** The locks and state of an active transaction */
 struct trx_lock_t;
-/** Transaction system */
-struct trx_sys_t;
 /** Signal */
 struct trx_sig_t;
 /** Rollback segment */

storage/innobase/read/read0read.cc
@@ -182,8 +182,8 @@ will mark their views as closed but not actually free their views.
 */
 void ReadView::snapshot(trx_t *trx)
 {
-  ut_ad(!mutex_own(&trx_sys.mutex));
   trx_sys.snapshot_ids(trx, &m_ids, &m_low_limit_id, &m_low_limit_no);
+  std::sort(m_ids.begin(), m_ids.end());
   m_up_limit_id= m_ids.empty() ? m_low_limit_id : m_ids.front();
   ut_ad(m_up_limit_id <= m_low_limit_id);
 }
@@ -219,7 +219,7 @@ void ReadView::open(trx_t *trx)
       protection. But we're cutting edges to achieve great scalability.
 
       There're at least two types of concurrent threads interested in this
-      value: purge coordinator thread (see MVCC::clone_oldest_view()) and
+      value: purge coordinator thread (see trx_sys_t::clone_oldest_view()) and
       InnoDB monitor thread (see lock_trx_print_wait_and_mvcc_state()).
 
       What bad things can happen because we allow this race?
@@ -319,10 +319,7 @@ void ReadView::close()
 */
 void trx_sys_t::clone_oldest_view()
 {
-  const ReadView *oldest_view= &purge_sys->view;
-
   purge_sys->view.snapshot(0);
-
   mutex_enter(&mutex);
   /* Find oldest view. */
   for (const ReadView *v= UT_LIST_GET_FIRST(m_views); v;
@@ -333,11 +330,8 @@ void trx_sys_t::clone_oldest_view()
     while ((state= v->get_state()) == READ_VIEW_STATE_SNAPSHOT)
       ut_delay(1);
 
-    if (state == READ_VIEW_STATE_OPEN &&
-        v->low_limit_no() < oldest_view->low_limit_no())
-      oldest_view= v;
+    if (state == READ_VIEW_STATE_OPEN)
+      purge_sys->view.copy(*v);
   }
-  if (oldest_view != &purge_sys->view)
-    purge_sys->view.copy(*oldest_view);
   mutex_exit(&mutex);
 }

storage/innobase/trx/trx0trx.cc
@@ -1222,10 +1222,7 @@ trx_serialise(trx_t* trx)
 		mutex_enter(&purge_sys->pq_mutex);
 	}
 
-	trx->no = trx_sys.get_new_trx_id();
-	my_atomic_store64_explicit(reinterpret_cast<int64*>
-				   (&trx->rw_trx_hash_element->no),
-				   trx->no, MY_MEMORY_ORDER_RELAXED);
+	trx_sys.assign_new_trx_no(trx);
 
 	/* If the rollack segment is not empty then the
 	new trx_t::no can't be less than any trx_t::no