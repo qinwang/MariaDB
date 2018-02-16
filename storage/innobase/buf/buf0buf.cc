9d55498af8d3aad6f6f28ca5895c3b24b4af85ea
Thirunarayanan Balathandayuthapani <thiru@mariadb.com>
2018-02-16 19:04:01 +0530

    -Follow up fix for mdev-15053

--- storage/innobase/buf/buf0buf.cc
+++ storage/innobase/buf/buf0buf.cc
@@ -5111,7 +5111,7 @@ buf_page_init_low(
 {
 	bpage->flush_type = BUF_FLUSH_LRU;
 	bpage->io_fix = BUF_IO_NONE;
-	ut_a(bpage->buf_fix_count = 0);
+	ut_a(bpage->buf_fix_count == 0);
 	bpage->old = 0;
 	bpage->freed_page_clock = 0;
 	bpage->access_time = 0;
