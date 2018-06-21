6c41186a3cf10b3a615b46e74e4f61061b950053
Alexey Botchkov <holyfoot@askmonty.org>
2018-06-21 16:27:47 +0400

    PING

--- sql/mysqld.cc
+++ sql/mysqld.cc
@@ -14,6 +14,7 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA */
 
+/*ping*/
 #include "sql_plugin.h"                         // Includes my_global.h
 #include "sql_priv.h"
 #include "unireg.h"
